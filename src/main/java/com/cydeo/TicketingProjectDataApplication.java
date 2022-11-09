26388a73f4580fd222dbc4a7426da62cc7c6b27d
yasinTawasulli <royesh14@gmail.com>
2022-11-09 12:37:12 -0800

    initial commit

--- src/main/java/com/cydeo/TicketingProjectDataApplication.java
+++ src/main/java/com/cydeo/TicketingProjectDataApplication.java
@@ -8,6 +8,7 @@ public class TicketingProjectDataApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TicketingProjectDataApplication.class, args);
+
     }
 
 }
